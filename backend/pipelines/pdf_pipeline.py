9f9c19f7eafc62d3326cf6c782d2008923900114
y-triki <ytriki1@myges.fr>
2025-07-14 03:34:35 +0200

    problème import OK

--- backend/pipelines/pdf_pipeline.py
+++ backend/pipelines/pdf_pipeline.py
@@ -1,17 +1,17 @@
 # pdf_pipeline.py
 import os
-import os
 import fitz
 from tokenizers import ByteLevelBPETokenizer
 from typing import Union
 import torch
+import re
 
 
 class PDFProcessor:
     def __init__(self, tokenizer_dir , max_len=1024, max_tokens_total=8192):
         # Construit un chemin ABSOLU à partir du fichier pdf_pipeline.py
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-        tokenizer_dir = os.path.join(BASE_DIR, '..', 'models', 'summary')
+        tokenizer_dir = os.path.join(BASE_DIR, '..', 'models', 'student_model')
         self.tokenizer = ByteLevelBPETokenizer(
             os.path.join(tokenizer_dir, "vocab.json"),
             os.path.join(tokenizer_dir, "merges.txt")
